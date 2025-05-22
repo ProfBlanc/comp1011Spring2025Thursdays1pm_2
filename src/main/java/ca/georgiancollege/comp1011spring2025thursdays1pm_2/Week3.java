f7285a58d07e0fb02f0d396ccc41d0906023e6d3
Ben <ben.blanc@georgebrown.ca>
2025-05-22 15:48:52 -0400

    Class Code May 22, 2025

---
+++ src/main/java/ca/georgiancollege/comp1011spring2025thursdays1pm_2/Week3.java
@@ -0,0 +1,26 @@
+package ca.georgiancollege.comp1011spring2025thursdays1pm_2;
+
+public class Week3 {
+
+    /*
+            Interface?
+                list of requirements: method's declared but not implemented
+
+            FunctionalInterface
+                1 abstract method
+                    needed to create lambda expressions
+                    can attach functional interface to 1 event
+
+        public void NameOfFunc(String str, int i, double d){System.out.println("hello");}
+
+                (str, i, d) -> {System.out.println("hello");}
+
+                (str, i, d) -> System.out.println("hello");
+
+
+                Interface interfaceName = new ClassThatImplementsInterface();
+
+
+    */
+
+}
